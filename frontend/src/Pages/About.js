ed783c286955a682b1cf55820e9ae2a2e3d2e5bd
Brandon Life <blife1119@gmail.com>
2023-11-19 05:17:03 -0500

    updated contact page

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -8,7 +8,7 @@ function About() {
         <h1>About Us</h1>
       </div>
       <div className='about-paragraph'>
-        <p>We are a society of secret agents. We are not here but are here. We come and we go. Time is irrelevant. Do you understand?
+        <p>We are a society of secret agents. We are here but not here. We come and we go. Time is irrelevant. Do you understand?
           If not we hope our website will help you to understand.
         </p>
       </div>
